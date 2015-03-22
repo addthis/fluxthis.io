ad5a7f58673e907040c459f3159c47120955f1ae
Josh Horwitz <horwitz@addthis.com>
2015-03-22 11:25:56 -0400

    Renamed invariants to common errors for clarity

--- src/components/docs/DocsNav.jsx
+++ src/components/docs/DocsNav.jsx
@@ -37,7 +37,7 @@ const navItems = [
     [
         {'debugging': 'Debugging'},
         {'testing': 'Test Utilities'},
-        {'invariants': 'Invariants'}
+        {'invariants': 'Common Errors'}
     ],
     [
         {'development': 'Development'}
